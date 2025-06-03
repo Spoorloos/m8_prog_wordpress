e06825f22402cf294b117547969f942c4d120a88
Spoorloos <mick.negenman@icloud.com>
2025-06-03 15:29:07 +0200

    Minor changes

--- themes/italian-recipes/functions.php
+++ themes/italian-recipes/functions.php
@@ -2,9 +2,10 @@
 
 function italian_recipes_enqueue_styles()
 {
-    $stylesheet_dir = get_stylesheet_directory_uri();
-    wp_enqueue_style('italian-recipes-generated', $stylesheet_dir . '/generated/style.css');
+    $stylesheet = get_stylesheet_directory_uri() . '/generated/style.css';
+    wp_enqueue_style('italian-recipes-generated', $stylesheet);
 }
 
 add_action('wp_enqueue_scripts', 'italian_recipes_enqueue_styles');
 add_theme_support('post-thumbnails');
+add_theme_support('title-tag');
